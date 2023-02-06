a36e499f1a34a767c4a2c7dd8ed17c82eec07c8a
nikjk <nik.kuehn@outlook.com>
2023-02-05 22:02:09 -0500

    Initial push for the Unit 3 LINQ Lab

--- Unit3-LINQLab/Program.cs
+++ Unit3-LINQLab/Program.cs
@@ -12,20 +12,24 @@ int maxMinusValue = maxValue - 10000;
 IEnumerable<int> smallValuesBetween = nums.Where(x => x > 10 && x < 100);
 IEnumerable<int> largeValuesBetween = nums.Where(x => x >= 100000 && x <= 999999);
 int evenCount = nums.Where(x => x % 2 == 0).Count();
+
 //Dispalying values for each question. 
 Console.WriteLine("\n");
+
 Console.Write("Minimum value in the array: " + minValue);
 Console.WriteLine("\n");
+
 Console.Write("Maximum value in the array: " + maxValue);
 Console.WriteLine("\n");
+
 Console.Write("The value of the Maximum - 10,000: " + maxMinusValue);
 Console.WriteLine("\n");
+
 Console.Write("Numbers in the array between 10 and 100: ");
 foreach (int num in smallValuesBetween)
 {
     Console.Write(num + ", ");
 }
-
 Console.WriteLine("\n");
 
 Console.Write("Numbers in the array between 100,000 and 999,999 (inclusive): ");
@@ -34,7 +38,6 @@ foreach (int num in largeValuesBetween)
 
     Console.Write(num + ", ");
 }
-
 Console.WriteLine("\n");
 
 Console.Write("Count of all even numbers in the array: " + evenCount);
